ef6105f9bcdb54b14998f785ae9e17a10b506ae6
Habu <habu1010+github@gmail.com>
2025-06-16 00:32:40 +0900

    solution: 06_ticket_management

--- exercises/06_ticket_management/13_index/src/lib.rs
+++ exercises/06_ticket_management/13_index/src/lib.rs
@@ -58,6 +58,22 @@ impl TicketStore {
     }
 }
 
+impl std::ops::Index<TicketId> for TicketStore {
+    type Output = Ticket;
+
+    fn index(&self, id: TicketId) -> &Self::Output {
+        self.get(id).unwrap()
+    }
+}
+
+impl std::ops::Index<&TicketId> for TicketStore {
+    type Output = Ticket;
+
+    fn index(&self, id: &TicketId) -> &Self::Output {
+        &self[*id]
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{Status, TicketDraft, TicketStore};
